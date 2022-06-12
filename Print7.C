6f4027e950ab39ad8abd8daf64e7bf2bee94c1e3
Shahidshameem <106670298+Shahidshameem@users.noreply.github.com>
2022-06-12 10:20:17 +0530

    Create Print7.C

---
+++ Print7.C
@@ -0,0 +1,22 @@
+// programe in C to print sum of two numbers.
+
+#include <stdio.h>
+
+int main()
+
+{
+
+ int a,b,c;
+
+ printf("enter the two numbers ");
+
+ scanf("%d%d",&a,&b);
+
+ c=a+b;
+
+ printf("the sum of %d and %d is %d",a,b,c);
+
+ return 0;
+
+}
+
