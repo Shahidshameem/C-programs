226eac175fd0cc2bd2e900a24ad8508e002a37bf
Shahidshameem <106670298+Shahidshameem@users.noreply.github.com>
2022-06-12 09:34:54 +0530

    Create Print3.C

---
+++ Print3.C
@@ -0,0 +1,48 @@
+// programe in C to print 3 names using switch.
+
+#include <stdio.h>
+
+int main()
+
+{
+
+ int a;
+
+ printf("enter value");
+
+ scanf("%d",&a);
+
+    switch(a)
+
+{
+
+ case 1:
+
+ printf("shahid");
+
+ break;
+
+ case 2:
+
+ printf("ubaid");
+
+ break;
+
+ case 3:
+
+ printf("basit");
+
+ break;
+
+default:
+
+     printf("entered value is invalid");
+
+}
+
+return 0;
+
+}
+
+ 
+
