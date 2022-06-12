529e7791fc4a97305de4a9ae8c21b07e4083f991
Shahidshameem <106670298+Shahidshameem@users.noreply.github.com>
2022-06-12 10:11:35 +0530

    Create Print6.C

---
+++ Print6.C
@@ -0,0 +1,26 @@
+// programe in C to check wheather a number is even or odd.
+
+#include <stdio.h>
+
+int main()
+
+{
+
+ int a;
+
+ printf("enter the integer ");
+
+ scanf("%d",&a);
+
+ if (a%2==0)
+
+ printf("the entered no.is even");
+
+ else
+
+ printf("the entered no.is odd");
+
+ return 0;
+
+}
+
