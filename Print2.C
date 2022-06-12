608f843941887ef986a91a3700d92ed7da0fbd6c
Shahidshameem <106670298+Shahidshameem@users.noreply.github.com>
2022-06-12 09:26:30 +0530

    Create Print2.C

---
+++ Print2.C
@@ -0,0 +1,15 @@
+// Programe in c to compare three distinct integers.
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    printf("please enter the number");
+    scanf("%d%d%d",&a,&b,&c);
+    if (a>b,a>c)
+    {
+      printf("%d is greatest",a);}
+      else if (b>c,b>a)
+   {  printf("%d is greatest",b);
+    }
+    return 0;
+}
