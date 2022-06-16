1c3616f0533e42722a1542c559effe68b3a2c6ea
Shahidshameem <106670298+Shahidshameem@users.noreply.github.com>
2022-06-16 22:12:55 +0530

    Create Print10.C

---
+++ Print10.C
@@ -0,0 +1,27 @@
+// programe in C to print numbers which are double that of previous number.
+
+#include <stdio.h>
+
+int main() {
+
+    int i,n;
+
+    printf("please enter the value of n: ");
+
+    scanf("%d",&n);
+
+    for(i=2;i<=512;i=i*2)
+
+    {
+
+        printf("%d \n",i);
+
+        
+
+    }
+
+    
+
+    return 0;
+
+}
